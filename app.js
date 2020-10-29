7f9aa48b5df60f040a13766419bed110ce6ace1e
vg0x0 <vikash.gupta@in.ibm.com>
2020-10-29 17:58:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from ICICI Prudential!");
+  res.send("Hello from ICICI Prudential Life Insurance!");
 });
  
 module.exports.app = app;
