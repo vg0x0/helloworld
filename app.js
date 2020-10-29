2ebc854900733068b8cf8a0d9b3501f03a7338d6
vg0x0 <vikash.gupta@in.ibm.com>
2020-10-29 17:09:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from IBM Cloud!");
+  res.send("Hello from ICICI Prudential!");
 });
  
 module.exports.app = app;
